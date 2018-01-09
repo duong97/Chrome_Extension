ef58687021c25af21acad6287338c9429c19bf3a
Duong <nvduong15@gmail.com>
2018-01-09 09:50:10 +0700

    First commit!
    commit all!

---
+++ fb4christmas/myscript.js
@@ -0,0 +1,4 @@
+var btn = document.createElement("img");
+btn.className = "onggianoelbay";
+btn.src = "https://vignette.wikia.nocookie.net/icarly/images/5/51/Animated-santa-reindeer.gif/revision/latest?cb=20111125195748";
+document.body.appendChild(btn);
---
+++ fb4christmas/popup.js
@@ -0,0 +1,4 @@
+/*  chrome.tabs.executeScript({
+    file: 'myscript.js'
+  }); 
+*/
